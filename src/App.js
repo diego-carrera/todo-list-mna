5b0faeb464fb1e61bc800d3ae5031cdb9dc66fdc
Diego Carrera Nicholls <diego.carrera@gmail.com>
2023-06-03 11:47:01 -0600

    Sprint 1

---
+++ src/App.js
@@ -0,0 +1,80 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from "react";
+import { nanoid } from "nanoid";
+
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+
+function App(props) {
+
+  const [tasks, setTasks] = React.useState(props.tasks);
+
+  const taskNoun = tasks.length !== 1 ? 'tareas' : 'tarea';
+  const headingText = `${tasks.length} ${taskNoun} pendientes`;
+
+
+  const taskList = tasks.map(task => (
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}` + tasks.length, name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>MNA Todo Tasker</h1>
+      <Form addTask={addTask}/>
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
